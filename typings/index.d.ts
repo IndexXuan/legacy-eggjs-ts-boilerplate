a08a102a2c33d3f2150f3de6f96b1d35b6854531
IndexXuan <indexxuan@gmail.com>
2023-09-26 14:22:40 +0800

    feat(typings): 补充 ctx.runInBackground

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,13 @@
 import 'egg';
+import { Context } from 'egg';
 
-declare module 'egg' {}
+declare module 'egg' {
+  interface Context {
+    /**
+     * Run async function in the background
+     * @see - https://eggjs.github.io/zh/guide/context.html#ctx-runinbackground
+     * 有实现，无类型，这里补充上
+     */
+    runInBackground(scope: () => void): void;
+  }
+}
